e9e31c96a000e85461599aa5c4d2f9a87f52b847
Boyd Kane <boydrkane@gmail.com>
2020-03-16 00:13:20 +0200

    updated crontab script

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 chmod 700 README.md gamestate.json init.sh judge.py layout.txt layout_template.txt map_template.html reset.py
+chmod 710 start_battle.sh
 chmod 744 bot.py index.html styles.css map.html
 chmod 755 -R bots bouncer.py requests.php 
