dca8748d45b10f43809f0d6a0fecbbf2929475b5
Boyd Kane <boydrkane@gmail.com>
2020-04-08 14:01:50 +0200

    Revert "Last revision of distributed Genghis"
    This reverts commit c6296339a347e3013ac02e0c16e906fbe42604fc.

--- node/requests.php
+++ node/requests.php
@@ -7,24 +7,21 @@ $cwd = getcwd();
 $genghis_dir = implode("/", array_slice(explode("/", $cwd), 0, -1));
 
 # add the incoming dictionary to file, for the judge system to validate and integrate into the game
+echo file_get_contents('php://input');
 $data = file_get_contents('php://input');
-echo count(array_keys((array)$data));
-if (count((array)$data)) {
-    $path = $genghis_dir.'/logs/'.date("c").'.json';
-    
-    $file_handle = fopen($path, 'w');
-    fwrite($file_handle, $data);
-    fclose($file_handle);
-    
-    # Change permissions on the file so the judge system can clean up later
-    chmod($path, 0777);
-    
-    # log the operation to file, for auditing purposes
-    $log_dir = $genghis_dir.'/logs/requests.log';
-    $log = PHP_EOL.date("c").$data.PHP_EOL;
-    file_put_contents($log_dir, $log, FILE_APPEND);
+$path = $genghis_dir.'/logs/'.date("c").'.json';
 
-} else {
-    echo "POST object is empty";
-}
+$file_handle = fopen($path, 'w');
+fwrite($file_handle, $data);
+fclose($file_handle);
+echo $data;
+
+# Change permissions on the file so the judge system can clean up later
+chmod($path, 0777);
+
+# log the operation to file, for auditing purposes
+$log_dir = $genghis_dir.'/logs/requests.log';
+$log = PHP_EOL.date("c").$data.PHP_EOL;
+file_put_contents($log_dir, $log, FILE_APPEND);
+
 ?>
